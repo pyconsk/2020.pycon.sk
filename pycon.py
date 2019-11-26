0bbb33c67fca3fa6fd65aa6aeb403320afb1381b
Juraj M. Bezrucka <mimmon@gmail.com>
2019-11-26 15:44:40 +0100

    added unique cover for each template

--- pycon.py
+++ pycon.py
@@ -25,6 +25,18 @@ CATEGORIES = {
 
 NEWS = get_news()
 
+COVERS = {
+    'cfp': 'img/about/speaker1.jpg',
+    'cfp_form': 'img/about/workshop.jpg',
+    'cfv': 'img/about/cfv.jpg',
+    'coc': 'img/about/chillout.jpg',
+    'index': 'img/about/header1.jpg',
+    'news': 'img/about/news.jpg',
+    'privacy': 'img/about/privacy.jpg',
+    'recording': 'img/about/snake.jpg',
+    'tickets': 'img/about/full_room.jpg',
+}
+
 
 @app.route('/sitemap.xml')
 def sitemap():
@@ -59,12 +71,14 @@ def root():
 
 @app.route('/<lang_code>/index.html')
 def index():
-    return render_template('index.html', **_get_template_variables(li_index='active', news=NEWS, categories=CATEGORIES))
+    return render_template('index.html', **_get_template_variables(li_index='active', news=NEWS, categories=CATEGORIES,
+                                                                   cover_name='index'))
 
 
 @app.route('/<lang_code>/news.html')
 def news():
-    return render_template('news.html', **_get_template_variables(li_news='active', news=NEWS, categories=CATEGORIES))
+    return render_template('news.html', **_get_template_variables(li_news='active', news=NEWS, categories=CATEGORIES,
+                                                                  cover_name='news'))
 
 
 @app.route('/<lang_code>/news/<category>.html')
@@ -84,47 +98,57 @@ def news_category(category):
 
 @app.route('/<lang_code>/coc.html')
 def coc():
-    return render_template('coc.html', **_get_template_variables(li_coc='active'))
+    return render_template('coc.html', **_get_template_variables(li_coc='active', cover_name='coc'))
 
 
 @app.route('/<lang_code>/tickets.html')
 def tickets():
-    return render_template('tickets.html', **_get_template_variables(li_tickets='active'))
+    return render_template('tickets.html', **_get_template_variables(li_tickets='active', cover_name='tickets'))
 
 
 @app.route('/<lang_code>/cfp.html')
 def cfp():
-    return render_template('cfp.html', **_get_template_variables(li_cfp='active'))
+    return render_template('cfp.html', **_get_template_variables(li_cfp='active', cover_name='cfp'))
 
 
 @app.route('/<lang_code>/cfp_form.html')
 def cfp_form():
-    return render_template('cfp_form.html', **_get_template_variables(li_cfp='active'))
+    return render_template('cfp_form.html', **_get_template_variables(li_cfp='active', cover_name='cfp_form'))
 
 
 @app.route('/<lang_code>/recording.html')
 def recording():
-    return render_template('recording.html', **_get_template_variables(li_recording='active'))
+    return render_template('recording.html', **_get_template_variables(li_recording='active', cover_name='recording'))
 
 
 @app.route('/<lang_code>/cfv.html')
 def cfv():
-    return render_template('cfv.html', **_get_template_variables(li_cfv='active'))
+    return render_template('cfv.html', **_get_template_variables(li_cfv='active', cover_name='cfv'))
 
 
 @app.route('/<lang_code>/thanks.html')
 def thanks():
-    return render_template('thanks.html', **_get_template_variables(li_cfp='active'))
+    return render_template('thanks.html', **_get_template_variables(li_cfp='active', cover_name='thanks'))
 
 
 @app.route('/<lang_code>/privacy-policy.html')
 def privacy_policy():
-    return render_template('privacy-policy.html', **_get_template_variables(li_privacy='active'))
+    return render_template('privacy-policy.html', **_get_template_variables(li_privacy='active', cover_name='privacy'))
 
 
 @app.route('/<lang_code>/countdown.html')
 def countdown():
-    return render_template('countdown.html', **_get_template_variables(li_index='active'))
+    return render_template('countdown.html', **_get_template_variables(li_index='active', cover_name='countdown'))
+
+
+def get_cover(name=None, path=None):
+    base_cover = 'img/about/header1.jpg'
+    if not name and not path:
+        path = base_cover
+    elif name:
+        path = COVERS.get(name, base_cover)
+    # todo check if path exists
+    return {'cover': path}
 
 
 def _get_template_variables(**kwargs):
@@ -139,6 +163,10 @@ def _get_template_variables(**kwargs):
         variables['lang_code'] = g.current_lang
     else:
         variables['lang_code'] = app.config['BABEL_DEFAULT_LOCALE']
+
+    cover_name = kwargs.get('cover_name')
+    if cover_name:
+        variables.update(**get_cover(name=cover_name))
 
     return variables
 
